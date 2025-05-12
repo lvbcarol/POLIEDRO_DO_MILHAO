189945597282e66901948b52e9ec56c71c58c8fb
fabio-png <74057935+fabio-png@users.noreply.github.com>
2025-05-12 16:28:07 -0300

    adicionando update questao (nao funcionando)

--- Backend/app/routes/questao.py
+++ Backend/app/routes/questao.py
@@ -6,11 +6,10 @@ import sqlite3
 questao_bp = Blueprint("questao", __name__)
 
 @questao_bp.route("/", methods=["POST"])
-@jwt_required()
 def adicionar_questao():
     data = request.get_json()
     # O ID_questao é AUTOINCREMENT, não precisa ser passado no request
-    # id_questao = data.get("id_questao") 
+    # id_questao = data.get("id_questao")
     enunciado = data.get("enunciado")
     alternativaA = data.get("alternativaA")
     alternativaB = data.get("alternativaB")
@@ -21,7 +20,7 @@ def adicionar_questao():
     valor = data.get("valor")
     nivel_dificuldade = data.get("nivel_dificuldade")
 
-    if not all([enunciado, alternativaA, alternativaB, alternativaC, alternativaD, resposta_correta]):
+    if not all([enunciado, alternativaA, alternativaB, alternativaC, alternativaD, resposta_correta, dica, valor, nivel_dificuldade]):
         return jsonify({"message": "Campos obrigatórios da questão estão faltando"}), 400
     
     if resposta_correta.upper() not in ["A", "B", "C", "D"]:
@@ -40,6 +39,72 @@ def adicionar_questao():
         return jsonify({"message": "Questão adicionada com sucesso!", "ID_questao": new_questao_id}), 201
     except Exception as e:
         return jsonify({"message": "Erro ao adicionar questão", "error": str(e)}), 500
+
+def buscar_questao_para_editar(id_questao):
+    db = get_db()
+    try:
+        questao_row = db.execute("SELECT * FROM questoes WHERE ID_questao = ?", (id_questao,)).fetchone()
+        if questao_row:
+            return True
+        else:
+            return False
+    except Exception as e:
+        return False
+
+@questao_bp.route("/", methods=["PUT"])
+def editar_questao():
+    data = request.get_json()
+    # O ID_questao é AUTOINCREMENT, não precisa ser passado no request
+    id_questao = data.get("ID_questao")
+    print(id_questao)
+
+    if not buscar_questao_para_editar(id_questao):
+        return jsonify({"message": "Questão não encontrada!"}), 400
+
+    enunciado = data.get("nova_enunciado")
+    alternativaA = data.get("nova_alternativaA")
+    alternativaB = data.get("nova_alternativaB")
+    alternativaC = data.get("nova_alternativaC")
+    alternativaD = data.get("nova_alternativaD")
+    resposta_correta = data.get("nova_resposta_correta")
+    dica = data.get("nova_dica")
+    valor = data.get("nova_valor")
+    nivel_dificuldade = data.get("novo_nivel_dificuldade")
+
+    valores = []
+    for valor in [enunciado, alternativaA, alternativaB, alternativaC, alternativaD, resposta_correta, dica, valor, nivel_dificuldade, id_questao]:
+        if valor is not None:
+            valores.append(valor)
+    if not any(valores):
+        return jsonify({"message": "Campos obrigatórios da questão estão faltando"}), 400
+    
+    if resposta_correta is not None and resposta_correta.upper() not in ["A", "B", "C", "D"]:
+        return jsonify({"message": "Resposta correta deve ser A, B, C ou D"}), 400
+
+    db = get_db()
+    updates = tuple(valores)
+    try:
+        cursor = db.execute(f"""
+            UPDATE questoes 
+            SET (
+                {'enunciado = ?,' if enunciado is not None else ''} 
+                {'alternativaA = ?,' if alternativaA is not None else ''}
+                {'alternativaB = ?,'if alternativaB is not None else ''}
+                {'alternativaC = ?, 'if alternativaC is not None else ''}
+                {'alternativaD = ?,'if alternativaD is not None else ''}
+                {'resposta_correta = ?,' if resposta_correta is not None else ''}
+                {'dica = ?,'if dica is not None else ''} 
+                {'valor = ?,'if valor is not None else ''} 
+                {'nivel_dificuldade = ?,'if nivel_dificuldade is not None else ''}
+                )
+            WHERE ID_questao = ?
+        """, updates)
+        db.commit()
+        new_questao_id = cursor.lastrowid
+        return jsonify({"message": "Questão adicionada com sucesso!", "ID_questao": new_questao_id}), 201
+    except Exception as e:
+        return jsonify({"message": "Erro ao adicionar questão", "error": str(e)}), 500
+
 
 @questao_bp.route("/", methods=["GET"])
 def listar_questoes():
@@ -63,8 +128,7 @@ def buscar_questao(id_questao):
     except Exception as e:
         return jsonify({"message": "Erro ao buscar questão", "error": str(e)}), 500
 
-@questao_bp.route("/check_answer", methods=["POST"])
-@jwt_required() # Geralmente verificar resposta é parte de um fluxo autenticado
+@questao_bp.route("/check_answer", methods=["POST"]) # Geralmente verificar resposta é parte de um fluxo autenticado
 def verificar_resposta():
     data = request.get_json()
     id_questao = data.get("id_questao")
